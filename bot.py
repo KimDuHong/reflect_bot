7f42bfede445611deb36e90c69583c62fb93a1ba
KimDuHong <art970@naver.com>
2024-01-23 02:32:29 +0900

    pwd log add

--- bot.py
+++ bot.py
@@ -158,9 +158,7 @@ async def on_message(message):
     elif "팔월드 서버 시작" == msg:
         msg = "서버를 시작합니다."
         await message.channel.send(msg)
-        print(os.system("cd .. && cd palworld-server-docker"))
-        print(os.system("pwd"))
-        print(os.system("docker-compose start"))
+        print(os.system("cd .. && cd palworld-server-docker && docker-compose start"))
 
     elif "팔월드 서버 종료" == msg:
         msg = "서버를 종료합니다."
